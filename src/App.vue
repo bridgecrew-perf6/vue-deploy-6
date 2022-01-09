eff164c075d8dbd44ca032f7b9697907fadead82
Gerard A. Jordan <gerard@Gerards-MacBook-Pro.local>
2022-01-09 15:58:09 +0100

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <h1>Hello, this is an H1 Title</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facere necessitatibus porro ad harum, assumenda voluptatibus itaque quasi eos accusantium iure error, vel qui perferendis numquam iusto facilis! Ea, tempore.</p>
+  <div class="my-40 max-w-3xl mx-auto antialiased">
+    <div class="flex flex-col space-y-10">
+      <h1 class="text-[5.625rem] leading-snug">On-Page SEO Basics</h1>
+      <p class="text-xl font-normal leading-normal">
+        On-page SEO (also known as “on-site SEO”) is the practice of optimizing web page content for search engines and users.
+        Common on-page SEO practices include optimizing title tags, content, internal links and URLs.
+      </p>
+    </div>
+    <div class="mt-20">
+      <h2 class="text-6xl leading-normal">All new features and updates</h2>
+      <div>
+        <p class="text-xl font-normal leading-normal">
+          A Live license is intended for a single user and cannot be shared.
+          Each license covers the use of the software on two active computers owned by the registered user.
+        </p>
+        <p class="text-xl font-normal leading-normal">
+          A new authorization may become necessary in the event of replacing essential hardware components (motherboard, hard drive, graphic card, etc.) or after an OS reinstallation.
+          If you run out of available authorizations, you can
+          <a href="" class="text-[#0000F5]">request more unlocks</a> provided you will only have Live installed on two machines in total.
+          You can find all the details about your rights as a licensed owner of Live in our End User License Agreement.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Futura PT', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
