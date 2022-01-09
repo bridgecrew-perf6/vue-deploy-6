28c92b86e619198a7dda226122aa7997752921a8
Gerard A. Jordan <gerard@Gerards-MacBook-Pro.local>
2022-01-09 14:55:22 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Hello, this is an H1 Title</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facere necessitatibus porro ad harum, assumenda voluptatibus itaque quasi eos accusantium iure error, vel qui perferendis numquam iusto facilis! Ea, tempore.</p>
+  </div>
+</template>
