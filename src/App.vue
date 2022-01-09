d352630f8eec44e31ab05d7498a87e85d4a3ff8f
Gerard A. Jordan <gerard@Gerards-MacBook-Pro.local>
2022-01-09 17:20:55 +0100

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="my-10 px-6 md:my-28 max-w-3xl mx-auto antialiased">
+  <div class="my-10 px-8 md:my-28 max-w-3xl mx-auto antialiased">
     <div class="flex flex-col space-y-10">
-      <h1 class="text-[5.625rem] leading-snug">On-Page SEO Basics</h1>
+      <h1 class="text-6xl lg:text-[5.625rem] leading-snug">On-Page SEO Basics</h1>
       <p class="text-xl font-normal leading-normal">
         On-page SEO (also known as “on-site SEO”) is the practice of optimizing web page content for search engines and users.
         Common on-page SEO practices include optimizing title tags, content, internal links and URLs.
       </p>
     </div>
     <div class="mt-20">
-      <h2 class="text-6xl leading-tight md:leading-normal">All new features and updates</h2>
-      <div class="mt-8">
-        <p class="text-xl font-normal leading-normal">
+      <h2 class="text-[2.5rem] lg:text-6xl leading-[1.4] md:leading-normal">All new features and updates</h2>
+      <div class="mt-5 lg:mt-8 text-lg space-y-5 ">
+        <p class="font-normal leading-normal">
           A Live license is intended for a single user and cannot be shared.
           Each license covers the use of the software on two active computers owned by the registered user.
         </p>
-        <p class="text-xl font-normal leading-normal">
+        <p class="font-normal leading-normal">
           A new authorization may become necessary in the event of replacing essential hardware components (motherboard, hard drive, graphic card, etc.) or after an OS reinstallation.
           If you run out of available authorizations, you can
           <a href="" class="text-[#0000F5]">request more unlocks</a> provided you will only have Live installed on two machines in total.
