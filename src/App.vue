f2c0c618d9713582d589d44fa0c8e27327d2d214
Gerard A. Jordan <gerard@Gerards-MacBook-Pro.local>
2022-01-23 14:09:52 +0100

    v1.02

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="my-10 px-8 md:my-28 max-w-3xl mx-auto antialiased">
     <div class="flex flex-col space-y-10">
-      <h1 class="text-6xl lg:text-[5.625rem] leading-snug">On-Page SEO Basics</h1>
+      <h1 class="text-6xl lg:text-[5.625rem] leading-snug">Vue deploy on Netlify and Vercel.</h1>
       <p class="text-xl font-normal leading-normal">
-        On-page SEO (also known as “on-site SEO”) is the practice of optimizing web page content for search engines and users.
-        Common on-page SEO practices include optimizing title tags, content, internal links and URLs.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio itaque, in possimus impedit iusto facilis quas recusandae nemo at et commodi officiis repellat quo, porro laudantium accusamus tempore corrupti voluptatem.
       </p>
     </div>
     <div class="mt-20">
       <h2 class="text-[2.5rem] lg:text-6xl leading-[1.4] md:leading-normal">All new features and updates</h2>
       <div class="mt-5 font-normal leading-normal text-lg lg:text-xl lg:mt-8 space-y-5 ">
         <p>
-          A Live license is intended for a single user and cannot be shared.
-          Each license covers the use of the software on two active computers owned by the registered user.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aliquam iste vel quis numquam alias magnam voluptatum non, quisquam assumenda adipisci consequuntur atque tempora commodi obcaecati fugit deserunt quibusdam id?
         </p>
         <p>
           A new authorization may become necessary in the event of replacing essential hardware components (motherboard, hard drive, graphic card, etc.) or after an OS reinstallation.
